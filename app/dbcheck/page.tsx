c9810fcce8b5981ef7e67dd30f41cf8ab531af0d
emilio <emiliojdeleon14@gmail.com>
2026-08-14 17:13:49 -0400

    remove db check

--- app/dbcheck/page.tsx
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless'
-
-export default async function Page() {
-  const sql = neon(process.env.DATABASE_URL!)
-  const [row] = await sql`select now()`
-  return <pre>{JSON.stringify(row)}</pre>
-}
